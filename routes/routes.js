376156a6a07d3f622f8dd91314ef0929b9370c61
Edwin Torres <CoachEd@gmail.com>
2017-07-03 15:32:59 -0400

    read all files that match the queried vrs num

--- routes/routes.js
+++ routes/routes.js
@@ -190,7 +190,7 @@ var appRouter = function(app,fs,ip,port) {
 							var retrievedJson = JSON.parse(fs.readFileSync(tpath + '/' + filename, 'utf8'));
 							filevrsnum = retrievedJson.custom_fields[1].value;
 							if (filevrsnum == vrsnum) {
-								console.log("adding: " + JSONT.stringify(retrievedJson));
+								console.log("adding: " + JSON.stringify(retrievedJson));
 								returnJson.push(JSON.parse(retrievedJson));
 							}
 						}
